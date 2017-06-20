31ddbc25da0c7a4750288ae830e3b964c81630ed
pinasd chen <pinasd.99@gmail.com>
2017-06-20 20:30:37 +0800

    Initial Commit

---
+++ Test3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test3
+//
+//  Created by pinasd chen on 2017/6/20.
+//  Copyright © 2017年 pinasd chen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
